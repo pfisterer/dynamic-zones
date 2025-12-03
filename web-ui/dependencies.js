7f70ac7473bafc54202d514425b258b4641ecf8f
Dennis Pfisterer <github@farberg.de>
2025-12-03 16:18:34 +0100

    Remove web-ui from the project, migrate to separate project

--- web-ui/dependencies.js
+++
@@ -1,8 +0,0 @@
-export * from 'preact';
-export * from 'htm/preact';
-export * from 'preact/hooks';
-export * from 'oidc-client-ts';
-export * from "wouter-preact";
-export * from "wouter-preact/use-hash-location";
-
-import 'bulma/css/bulma.min.css';
